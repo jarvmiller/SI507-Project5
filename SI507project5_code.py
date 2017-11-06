6d551a350d5caf490a76a688a7a96f08607ffd1c
Jarvis Miller <jarvm@Jarviss-MacBook-Pro.local>
2017-11-06 09:56:40 -0500

    done. just need to refactor

--- SI507project5_code.py
+++ SI507project5_code.py
@@ -1,4 +1,3 @@
-## import statements
 import webbrowser
 import json
 import requests
@@ -11,25 +10,26 @@ from eb_data import CLIENT_ID, CLIENT_SECRET, personal_token
 AUTHORIZATION_URL = 'https://www.eventbrite.com/oauth/authorize'
 TOKEN_URL = 'https://www.eventbrite.com/oauth/token'
 REDIRECT_URI = 'https://www.programsinformationpeople.org/runestone/oauth'
-## CACHING SETUP
 
-CACHE_FNAME = "cache_contents.json"
-CREDS_CACHE_FILE = "creds.json"
+HARVEY_CACHE_FNAME = "harvey_cache_contents.json"
+CONCERT_CACHE_FNAME = "concert_cache_contents.json"
 
 #--------------------------------------------------
 # Load cache files: data and credentials
 #--------------------------------------------------
 # Load data cache
-try:
-    with open(CACHE_FNAME, 'r') as cache_file:
-        cache_json = cache_file.read()
-        CACHE_DICTION = json.loads(cache_json)
-except:
-    CACHE_DICTION = {}
+def check_if_cached(fname):
+    try:
+        with open(fname, 'r') as cache_file:
+            cache_json = cache_file.read()
+            CACHE_DICTION = json.loads(cache_json)
+    except:
+        CACHE_DICTION = {}
+    return CACHE_DICTION
 
+HARVEY_CACHE_DICTION = check_if_cached(HARVEY_CACHE_FNAME)
+CONCERT_CACHE_DICTION = check_if_cached(CONCERT_CACHE_FNAME)
 
-## ADDITIONAL CODE for program should go here...
-## Perhaps authentication setup, functions to get and process data, a class definition... etc.
 
 def get_saved_token():
     with open('token.json', 'r') as f:
@@ -38,63 +38,61 @@ def get_saved_token():
 
         return token_dict
 
+
 def save_token(token_dict):
     with open('token.json', 'w') as f:
         token_json = json.dumps(token_dict)
         f.write(token_json)
 
-def get_eventbrite_cache(search_params, force_download=False, CACHE_DICTION=CACHE_DICTION)
-    if CACHE_DICTION == {} or force_download == True:
-        # Set up sessions and so on to get data via OAuth2 protocol...
+
+def get_eventbrite_cache(search_params, CACHE_DICTION, CACHE_FNAME, force_download=False):
+    # if we need to get an oauth2 session started
+    if CACHE_DICTION == {} or force_download:
+        # see if we have the token
         try:
             token = get_saved_token()
         except FileNotFoundError:
             token = None
 
-        if token and force_download==False:
-            print('token already saved, just retrieved it')
+        if token and not force_download:
+            print('Token already saved, just retrieved it')
             oauth2inst = requests_oauthlib.OAuth2Session(CLIENT_ID, token=token)
         else:
-            print('getting token the long way')
+            print('Getting token the long way')
             oauth2inst = requests_oauthlib.OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI) # Create an instance of an OAuth2Session
 
             # get the authorization url to send the user to
             authorization_url, state = oauth2inst.authorization_url(AUTHORIZATION_URL)
 
-            webbrowser.open(authorization_url) # Opening auth URL for you to sign in to the Spotify service
+            # Opening auth URL for you to sign in to the EventBrite service
+            webbrowser.open(authorization_url) 
             authorization_response = input('Authenticate and then enter the full callback URL: ').strip() # Need to get the full URL in order to parse the response
 
-            # The OAuth2Session instance has a method that extracts what we need from the url, and helps do some other back and forth with spotify
+            # The OAuth2Session instance has a method that extracts what we need from the url, and helps do some other back and forth with EB
             token = oauth2inst.fetch_token(TOKEN_URL, authorization_response=authorization_response, client_secret=CLIENT_SECRET)
             save_token(token)
         
 
 
-        print('getting owned events after obtaining token')
-        r = oauth2inst.get('https://www.eventbriteapi.com/v3/events/search/', params=EB_search_params)
-        # takes r.text and makes it into a dictionary
-        # print(r.text)
+        print('Token saved. Getting search results')
+        r = oauth2inst.get('https://www.eventbriteapi.com/v3/events/search/', params=search_params)
+
+        # the result is now a dictionary
         response_diction = json.loads(r.text)
-        # print(response_diction['events'], [event['id'] for event in response_diction['events']])
-        # print(response_diction['events'][0]['id'])
-        # print(json.dumps(response_diction, indent=2)) # See the response printed neatly -- uncomment
 
         with open(CACHE_FNAME, 'w') as cache_file:
-            print('saving event json as', CACHE_FNAME)
+            print('caching result as:', CACHE_FNAME)
             for event in response_diction['events']:
                 CACHE_DICTION[event['id']] = event
             cache_json = json.dumps(CACHE_DICTION, indent=2)
             cache_file.write(cache_json)
     else:
-        print('owned events already saved as cache')
+        print("{} already saved as cache, will return it".format(CACHE_FNAME))
     
     return CACHE_DICTION
 
-harvey_search_params = {'q':'Hurricane Harvey', "location.address":'6100 Main St, Houston, TX 77005',
-                            'location.within':'30mi'}
 
-CACHE_DICTION = get_eventbrite_cache(harvey_search_params, "location.address":'6100 Main St, Houston, TX 77005',
-                            'location.within':'30mi'})
+
 
 class Event(object):
     def __init__(self, event_dict):
@@ -103,7 +101,7 @@ class Event(object):
         self.name = self.event.get('name', {}).get('text')
 
         self.capacity = self.event.get('capacity')
-        self.uri = self.event.get('resource_uri')
+        self.url = self.event.get('url')
         self.is_free = self.event.get('is_free')
         self.description = self.event.get('description', {}).get('text')
 
@@ -118,14 +116,11 @@ class Event(object):
                 self.data[','.join(key)] = reduce(dict.get, key, self.event)
             except:
                 self.data[','.join(key)] = None
-        # print(self.data)
 
     def __str__(self):
         return "{0}: {1}".format(self.id, self.name)
 
-event_list = [Event(event_dict) for event_dict in CACHE_DICTION.values()]
 
-# [event.get_data() for event in event_list]
 def write_to_csv(event_list, filename):
     with open(filename, 'w') as outfile:
         outwriter = csv.writer(outfile, delimiter=',')
@@ -137,8 +132,29 @@ def write_to_csv(event_list, filename):
             row = list(event.data.values())
             outwriter.writerow(row)
 
-write_to_csv(event_list, 'harvey.csv')
+
+if __name__ == '__main__':
+    harvey_search_params = {'q':'Hurricane Harvey',
+                            "location.address":'6100 Main St, Houston, TX 77005',
+                            'location.within':'30mi'}
+    concert_search_params = {'q':'concert',
+                             'location.address': "500 S State St, Ann Arbor, MI 48109",
+                             'location.within':'20mi'}
+
+    HARVEY_CACHE_DICTION = get_eventbrite_cache(harvey_search_params, 
+                                                HARVEY_CACHE_DICTION,
+                                                HARVEY_CACHE_FNAME)
+    CONCERT_CACHE_DICTION = get_eventbrite_cache(concert_search_params,
+                                                 CONCERT_CACHE_DICTION,
+                                                 CONCERT_CACHE_FNAME)
+
+    harvey_event_list = [Event(event_dict) for event_dict in HARVEY_CACHE_DICTION.values()]
+    concert_event_list = [Event(event_dict) for event_dict in CONCERT_CACHE_DICTION.values()]
+    print('writing to csv')
+    write_to_csv(harvey_event_list, 'harvey.csv')
+    write_to_csv(concert_event_list, 'um_concert.csv')
 
 # to do:
 # make a separate cache file, one for harvey, one for A2 concerts
+
 ## Make sure to run your code and write CSV files by the end of the program.
--- SI507project5_tests.py
+++ SI507project5_tests.py
@@ -1,18 +1,50 @@
 import unittest
+import json
 from SI507project5_code import *
 
 
+class TEST_CACHE(unittest.TestCase):
+    def test_cache_type(self):
+        self.assertTrue(isinstance(HARVEY_CACHE_DICTION, dict))
+        self.assertTrue(isinstance(CONCERT_CACHE_DICTION, dict))
 
 
 
+class TEST_EVENT(unittest.TestCase):
+    def setUp(self):
+        with open('test_cache_contents.json', 'r') as f:
+            cache_json = f.read()
+            event_diction = json.loads(cache_json)
+        self.event_list = [Event(event) for event in event_diction.values()]
+        self.event = self.event_list[0]
+
+    def test_event_list(self):
+        [self.assertTrue(isinstance(event, Event)) for event in self.event_list]
+
+    def test_init(self):
+        self.assertTrue(self.event.id == '39130956745')
+        self.assertTrue(self.event.name == 'Hurricane Harvey Benefit Concert')
+        self.assertFalse(self.event.is_free)
+        self.assertTrue(self.event.url == "https://www.eventbrite.com/e/hurricane-harvey-benefit-concert-tickets-39130956745?aff=ebapi")
 
 
+    def test_str(self):
+        self.assertEqual(self.event.__str__(),
+                         "39130956745: Hurricane Harvey Benefit Concert")
 
 
+class TEST_CSVS(unittest.TestCase):
+    def setUp(self):
+        self.harvey = open('harvey.csv')
+        self.concert = open('um_concert.csv')
 
+    def test_csv_files_exist(self):
+        self.assertTrue(self.harvey.read())
+        self.assertTrue(self.concert.read())
 
-
-
+    def tearDown(self):
+        self.harvey.close()
+        self.concert.close()
 
 
 if __name__ == "__main__":
